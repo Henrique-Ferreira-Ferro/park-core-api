9fb3bc82abc28f7a40104e11e5463dce7897d27a
Henrique Ferreira Ferro <henriqueferreiraferro@gmail.com>
2024-08-13 21:48:57 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/ParqueCore/ParkBeto/ParkBetoApplication.java
@@ -0,0 +1,13 @@
+package com.ParqueCore.ParkBeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkBetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkBetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ParqueCore/ParkBeto/ParkBetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ParqueCore.ParkBeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkBetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
