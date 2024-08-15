237c5a4cf759ed32fc7c8289bae5471f8fae6fac
Jhonatan Santos <jhonatansantos085@gmail.com>
2024-08-15 19:38:37 -0300

    Adicionei metodo de criar atrações

--- src/main/java/com/ParqueCore/ParkBeto/controller/AtracaoController.java
+++ src/main/java/com/ParqueCore/ParkBeto/controller/AtracaoController.java
@@ -1,8 +1,11 @@
 package com.ParqueCore.ParkBeto.controller;
 
 
+import com.ParqueCore.ParkBeto.model.Atracao;
 import com.ParqueCore.ParkBeto.service.AtracaoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +15,10 @@ public class AtracaoController {
 
     @Autowired
     private AtracaoService atracaoService;
+
+    @PostMapping
+    public Atracao create(@RequestBody Atracao atracao){
+        return atracaoService.save(atracao);
+    }
 
 }
--- src/main/java/com/ParqueCore/ParkBeto/service/AtracaoService.java
+++ src/main/java/com/ParqueCore/ParkBeto/service/AtracaoService.java
@@ -1,6 +1,7 @@
 package com.ParqueCore.ParkBeto.service;
 
 
+import com.ParqueCore.ParkBeto.model.Atracao;
 import com.ParqueCore.ParkBeto.repository.AtracaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,5 +11,9 @@ public class AtracaoService {
 
     @Autowired
     private AtracaoRepository atracaoRepository;
+
+    public Atracao save(Atracao atracao){
+        return atracaoRepository.save(atracao);
+    }
 
 }
