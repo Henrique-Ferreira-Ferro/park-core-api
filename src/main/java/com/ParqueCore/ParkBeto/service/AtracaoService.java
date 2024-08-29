82c16dc31046350cd5eb455a4ff2bf9906fccb24
Jhonatan Santos <jhonatansantos085@gmail.com>
2024-08-29 12:50:08 -0300

    Modifiquei a funcao de criar atracoes

--- src/main/java/com/ParqueCore/ParkBeto/service/AtracaoService.java
+++ src/main/java/com/ParqueCore/ParkBeto/service/AtracaoService.java
@@ -2,6 +2,7 @@ package com.ParqueCore.ParkBeto.service;
 
 import com.ParqueCore.ParkBeto.model.Atracao;
 import com.ParqueCore.ParkBeto.repository.AtracaoRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,9 +15,18 @@ public class AtracaoService {
     public void saveAtracao(Atracao atracao){
         this.atracaoRepository.save(atracao);
     }
+
+    @Transactional
     public Atracao createAtracao(Atracao atracao){
-        this.saveAtracao(atracao);
-        return atracao;
+        if(isNomeUnique(atracao.getNome())){
+            this.saveAtracao(atracao);
+            return atracao;
+        }else{
+            throw new IllegalArgumentException("A atracao ja foi cadastrada");
+        }
+    }
+    private boolean isNomeUnique(String nome) {
+        return !atracaoRepository.existsByName(nome);
     }
 
 }
