534fee9c485fe0e9dad5c0369aaca5e95fc1f276
Henrique Ferreira Ferro <henriqueferreiraferro@gmail.com>
2024-09-01 09:06:06 -0300

    Criação das funcionalidades voltadas para o cpf

--- src/main/java/com/ParqueCore/ParkBeto/controller/VisitanteController.java
+++ src/main/java/com/ParqueCore/ParkBeto/controller/VisitanteController.java
@@ -1,9 +1,12 @@
 package com.ParqueCore.ParkBeto.controller;
 
+import com.ParqueCore.ParkBeto.model.Visitante;
 import com.ParqueCore.ParkBeto.service.VisitanteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +16,12 @@ public class VisitanteController {
 
     @Autowired
     private VisitanteService visitanteService;
-
+    
+    @PostMapping("/cadastrar")
+    public Visitante cadastrarVisitante(@RequestBody Visitante visitante) {
+    	return visitanteService.cadastrarVisitante(visitante);
+    }
+    
     @DeleteMapping
     public void excluirVisitante(@PathVariable Long visitanteId) {
         visitanteService.excluirVisitante(visitanteId);
--- src/main/java/com/ParqueCore/ParkBeto/service/VisitanteService.java
+++ src/main/java/com/ParqueCore/ParkBeto/service/VisitanteService.java
@@ -1,5 +1,7 @@
 package com.ParqueCore.ParkBeto.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,20 +18,22 @@ public class VisitanteService {
     private VisitanteRepository visitanteRepository;
 
     public Visitante cadastrarVisitante(Visitante visitante) {
-    	
-    	if(validaCpf(visitante.getCpf()) == false) {
+        
+    	if(!validaCpf(visitante.getCpf())) {
     		throw new RuntimeException("CPF invalido! Digite um CPF valido!");
+    	}
+    	
+    	if(verificaCpf(visitante.getCpf()) == true) {
+    		throw new RuntimeException("O CPF já está cadastrado no sistema!");
     	}
     	
     	var visi = visitanteRepository.save(visitante);
     	
     	return visi;
     }
-    
-    
-    
-    
-    public void excluirVisitante(Long visitanteId) {
+   
+
+	public void excluirVisitante(Long visitanteId) {
         var visitante = visitanteRepository.findById(visitanteId)
                 .orElseThrow(() -> new EntityNotFoundException("Visitante não encontrado"));
 
@@ -49,18 +53,22 @@ public class VisitanteService {
      * https://medium.com/@edibertosilva33/validar-cpf-em-java-30be89fbc066
      * https://www.youtube.com/watch?v=d2uqo6PhdM4
      */
+	
+	
+	//Metodos não relacionados ao crud da aplicação!!!!!!!!
     public boolean validaCpf(String cpf) {
+    	    	
     	
-    	cpf = cpf.replace("-", "");
-    	cpf = cpf.replace(".", "");
     	
     	if(cpf.length() != 11 || cpf.equals("00000000000") || cpf.equals("11111111111") ||
         		cpf.equals("22222222222") || cpf.equals("33333333333") || cpf.equals("44444444444")
         		|| cpf.equals("55555555555") || cpf.equals("66666666666") || cpf.equals("77777777777")
-        		|| cpf.equals("88888888888") || cpf.equals("99999999999") || cpf.equals("01234567890")) {
+        		|| cpf.equals("88888888888") || cpf.equals("99999999999") || cpf.equals("01234567890")
+        		|| !cpf.matches("(\\d{11})")
+    			) {
         		return false;
         	}
-    	
+    	//123456789201
     	int totalSoma = 0;
     	for(int i = 0; i < 9; i++) {
     		totalSoma += Character.getNumericValue(cpf.charAt(i)) * (10 - i);
@@ -96,6 +104,21 @@ public class VisitanteService {
     	}else {
     		return false;
     	}
+    }
+    
+ // Metodo para verficar se um Cpf já existe no sistema
+    
+    private boolean verificaCpf(String cpf) {
+    	List<Visitante> visitantes = visitanteRepository.findAll();
+    	
+    	for (Visitante visitante : visitantes) {
+			if(visitante.getCpf().equals(cpf)) {
+				return true;
+			}
+		}
+    	
+    	return false;
+    	
     }
     
     
