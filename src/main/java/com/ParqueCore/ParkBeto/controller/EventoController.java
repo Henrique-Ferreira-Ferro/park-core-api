32ccef1eb3269ffb16f30d4c2db8d6968294c135
Jhonatan Santos <jhonatansantos085@gmail.com>
2024-09-14 19:37:02 -0300

    consertei bug na EventoController

--- src/main/java/com/ParqueCore/ParkBeto/controller/EventoController.java
+++ src/main/java/com/ParqueCore/ParkBeto/controller/EventoController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class EventoController {
 
 
 	@PostMapping
-	public ResponseEntity<Evento> createEvento(@PostMapping Evento evento){
+	public ResponseEntity<Evento> createEvento(@RequestBody Evento evento){
 		Evento newEvento = eventoService.createEvento(evento);
 		return new ResponseEntity<>(newEvento, HttpStatus.CREATED);
 	}
