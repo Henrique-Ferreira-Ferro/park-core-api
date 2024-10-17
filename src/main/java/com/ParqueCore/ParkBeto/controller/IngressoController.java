bc7fb9622bd604dc8c12d5306246a0d8a3d43af1
GuiRuchell <ruchellguilherme100@gmail.com>
2024-10-16 23:01:49 -0300

    implementando funcionalidades para o gerenciamento de ingressos

--- src/main/java/com/ParqueCore/ParkBeto/controller/IngressoController.java
+++ src/main/java/com/ParqueCore/ParkBeto/controller/IngressoController.java
@@ -1,16 +1,37 @@
 package com.ParqueCore.ParkBeto.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import com.ParqueCore.ParkBeto.model.Ingresso;
 import com.ParqueCore.ParkBeto.service.impl.IngressoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/ingresso")
 public class IngressoController {
 
-	@Autowired
-	private IngressoService ingressoService;
-	
+	private final IngressoService ingressoService;
+
+	public IngressoController(IngressoService ingressoService) {
+		this.ingressoService = ingressoService;
+	}
+
+	@PostMapping
+	public ResponseEntity<Ingresso> emitirIngresso(@RequestBody Ingresso ingressoRequest) {
+		var ingresso = ingressoService.emitirIngresso(ingressoRequest);
+		return ResponseEntity.ok(ingresso);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> cancelarIngresso(@PathVariable Long id) {
+		ingressoService.cancelarIngresso(id);
+		return ResponseEntity.ok("Ingresso cancelado com sucesso!");
+	}
+
+	@GetMapping
+	public ResponseEntity<List<Ingresso>> listarIngressos() {
+		var ingressos = ingressoService.listarIngressos();
+		return ResponseEntity.ok(ingressos);
+	}
 }
--- src/main/java/com/ParqueCore/ParkBeto/service/impl/IngressoService.java
+++ src/main/java/com/ParqueCore/ParkBeto/service/impl/IngressoService.java
@@ -1,17 +1,61 @@
 package com.ParqueCore.ParkBeto.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.ParqueCore.ParkBeto.model.Ingresso;
+import com.ParqueCore.ParkBeto.repository.AtracaoRepository;
+import com.ParqueCore.ParkBeto.repository.IngressoRepository;
+import com.ParqueCore.ParkBeto.repository.VisitanteRepository;
 import org.springframework.stereotype.Service;
 
-import com.ParqueCore.ParkBeto.repository.IngressoRepository;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class IngressoService {
-	
-	@Autowired
-	private IngressoRepository ingressoRepository;
 
-	
-	
-	
+	private final IngressoRepository ingressoRepository;
+	private final VisitanteRepository visitanteRepository;
+	private final AtracaoRepository atracaoRepository;
+
+	public IngressoService(IngressoRepository ingressoRepository, VisitanteRepository visitanteRepository, AtracaoRepository atracaoRepository) {
+		this.ingressoRepository = ingressoRepository;
+		this.visitanteRepository = visitanteRepository;
+		this.atracaoRepository = atracaoRepository;
+	}
+
+	public Ingresso emitirIngresso(Ingresso ingressoRequest) {
+		var visitante = visitanteRepository.findById(ingressoRequest.getVisitante().getId())
+				.orElseThrow(() -> new RuntimeException("Visitante não encontrado"));
+
+		var atracao = atracaoRepository.findById(ingressoRequest.getAtracao().getId())
+				.orElseThrow(() -> new RuntimeException("Atracao nao encontrada"));
+
+		var ingresso = new Ingresso();
+
+		ingresso.setVisitante(visitante);
+		ingresso.setAtracao(atracao);
+		ingresso.setDataEmissao(new Date());
+		ingresso.setTipo(ingressoRequest.getTipo());
+		ingresso.setStatus(ingressoRequest.getStatus());
+		ingresso.setCodigo(UUID.randomUUID().toString());
+		ingresso.setDataVisita(ingressoRequest.getDataVisita());
+
+		return ingressoRepository.save(ingresso);
+
+	}
+
+	public void cancelarIngresso(Long id) {
+		var ingresso = ingressoRepository.findById(id)
+				.orElseThrow(() -> new RuntimeException("ingresso não encontrado"));
+
+		if (!ingresso.getStatus().equals("ativo")) {
+			throw new RuntimeException("Ingresso não pode ser cancelado");
+		}
+		ingresso.setStatus("cancelado");
+		ingressoRepository.save(ingresso);
+	}
+
+	public List<Ingresso> listarIngressos() {
+		return ingressoRepository.findAll();
+	}
 }
