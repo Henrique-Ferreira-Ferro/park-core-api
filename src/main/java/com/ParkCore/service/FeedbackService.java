97a97061766560501ddc6d74d021df4b642ba359
Mailon augusto <augustomailon4@gmail.com>
2024-11-09 20:30:25 -0300

    Adicionada nova funcionalidades de feedback

--- src/main/java/com/ParkCore/service/FeedbackService.java
+++ src/main/java/com/ParkCore/service/FeedbackService.java
@@ -1,10 +1,17 @@
 package com.ParkCore.service;
 
 import com.ParkCore.exceptions.BadRequestException;
+import com.ParkCore.model.Attraction;
 import com.ParkCore.model.Feedback;
+import com.ParkCore.model.Visitor;
 import com.ParkCore.repository.FeedbackRepository;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 public class FeedbackService {
@@ -17,14 +24,53 @@ public class FeedbackService {
 
 	@Transactional
 	public Feedback realizarFeedback(Feedback feedback) {
-		validarFeedback(feedback);
+		validateFeedback(feedback);
 
 		return feedbackRepository.save(feedback);
 	}
 
-	private void validarFeedback(Feedback feedback) {
+	private void validateFeedback(@org.jetbrains.annotations.NotNull Feedback feedback) {
 		if (feedback.getComment() == null || feedback.getComment().isEmpty()) {
 			throw new BadRequestException("O comentário do feedback não pode ser vazio.");
 		}
+
+
+		@Setter
+        @Getter
+        @Entity
+		@Table(name = "feedback" )
+        class feedback{
+
+            @GeneratedValue(strategy = GenerationType.IDENTITY)
+			@Column(name = "id_avaliacao")
+			private Long idAvaliacao;
+
+			@ManyToOne
+			@JoinColumn(name = "id_visitor", referencedColumnName = "id_visitor")
+			private Visitor visitor;
+
+			@ManyToOne
+			@JoinColumn(name = "id_atracion", referencedColumnName = "id_atracion")
+			private Attraction attraction;
+
+			@Column(name = "text_avaliacion", length = 1000)
+			private String textoAvaliacao;
+
+			@Column(name = "classificacion")
+			private int classificacao;
+
+			@Column(name = "classificacion")
+			private LocalDateTime CreatDate;
+
+
+			public Object Feedback (LocalDateTime CreatDate) {
+                //noinspection DataFlowIssue
+                return CreatDate = CreatDate;
+			}
+
+        }
+
+
+
 	}
 }
